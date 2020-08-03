57f952ce123b4144a4fd7e6a17e6cd89c3f10977
BuildTools <unconfigured@null.spigotmc.org>
2020-08-03 23:49:54 +0800

    3/8/2020 23:49 -Remove login page AppBar

--- lib/authentication/login/login_page.dart
+++ lib/authentication/login/login_page.dart
@@ -15,7 +15,6 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
       body: BlocProvider<LoginBloc>(
         create: (context) => LoginBloc(userRepository: _userRepository),
         child: LoginForm(userRepository: _userRepository),
